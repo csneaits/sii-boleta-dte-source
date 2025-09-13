955d7b5d20f905b1c6ce2a0dcdf39794a6df8194
Carlos Eduardo Rodriguez Perez <42185068+eduard1859@users.noreply.github.com>
2025-09-12 21:06:36 -0300

    Move legacy includes into src directory

--- sii-boleta-dte/tests/bootstrap.php
+++ sii-boleta-dte/tests/bootstrap.php
@@ -4,7 +4,7 @@ define('ABSPATH', __DIR__.'/../');
 define('SII_BOLETA_DTE_PATH', __DIR__.'/../');
 define('SII_BOLETA_DTE_URL', 'http://example.com/');
 define('SII_BOLETA_DTE_VERSION', 'test');
-require SII_BOLETA_DTE_PATH . 'includes/autoload.php';
+require SII_BOLETA_DTE_PATH . 'src/includes/autoload.php';
 if ( file_exists( SII_BOLETA_DTE_PATH . 'vendor/autoload.php' ) ) {
     require SII_BOLETA_DTE_PATH . 'vendor/autoload.php';
 }
